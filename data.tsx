b5b08f759422310e7dd4770c49db56ed91f3a6c5
Hidekel Sevilla <hidekel.sevilla.morlet@gmail.com>
2024-06-03 19:14:33 -0600

    Proyectos añadidos

--- data.tsx
+++ data.tsx
@@ -96,103 +96,33 @@ export const dataCounter = [
   },
 ];
 
-export const serviceData = [
-  {
-    icon: <Crop />,
-    title: "Branding",
-    description:
-      "Desarrollo de una identidad de marca sólida y coherente, incluyendo diseño de logotipo, colores y elementos visuales",
-  },
-  {
-    icon: <Pencil />,
-    title: "Diseño web",
-    description:
-      "Diseño creativo y profesional de interfaces web intuitivas y atractivas, centradas en la experiencia del usuario",
-  },
-  {
-    icon: <Computer />,
-    title: "Desarrollo web",
-    description:
-      "Diseño y desarrollo de sitios web a medida, adaptados a tus necesidades",
-  },
-  {
-    icon: <Book />,
-    title: "Copywriting",
-    description:
-      "Creación de contenido persuasivo y atractivo que capta la atención de tu audiencia",
-  },
-  {
-    icon: <Rocket />,
-    title: "SEO",
-    description:
-      "Optimización de tu presencia en línea mediante estrategias de SEO avanzadas, ",
-  },
-];
-
 export const dataPortfolio = [
   {
     id: 1,
-    title: "Web Pro",
-    image: "/image-1.jpg",
-    urlGithub: "#!",
-    urlDemo: "#!",
+    title: "Primer Portfolio",
+    image: "/image-0.png",
+    urlGithub: "https://github.com/HidekelSevilla/PortfolioHidevkl",
+    urlDemo: "https://hidekelsevilla.github.io/PortfolioHidevkl/",
   },
   {
     id: 2,
-    title: "Desarrollo Web Ágil",
-    image: "/image-2.jpg",
+    title: "Proyecto 2",
+    image: "/sintítulo-1.png",
     urlGithub: "#!",
     urlDemo: "#!",
   },
   {
     id: 3,
-    title: "Estrategias Web",
-    image: "/image-3.jpg",
+    title: "Proyecto 3",
+    image: "/sintítulo-1.png",
     urlGithub: "#!",
     urlDemo: "#!",
   },
   {
     id: 4,
-    title: "Ideas Creativas",
-    image: "/image-4.jpg",
+    title: "Proyecto 4",
+    image: "/sintítulo-1.png",
     urlGithub: "#!",
     urlDemo: "#!",
-  },
-  {
-    id: 5,
-    title: "Webs Impactantes",
-    image: "/image-5.jpg",
-    urlGithub: "#!",
-    urlDemo: "#!",
-  },
-  {
-    id: 6,
-    title: "Web Dinámica",
-    image: "/image-6.jpg",
-    urlGithub: "#!",
-    urlDemo: "#!",
-  },
-  {
-    id: 7,
-    title: "Dark Web ",
-    image: "/image-7.jpg",
-    urlGithub: "#!",
-    urlDemo: "#!",
-  },
-  {
-    id: 8,
-    title: "E-commerce web",
-    image: "/image-8.jpg",
-    urlGithub: "#!",
-    urlDemo: "#!",
-  },
-];
-
-export const dataTestimonials = [
-  {
-    id: 1,
-    name: "",
-    description: "",
-    imageUrl: "",
   },
 ];
